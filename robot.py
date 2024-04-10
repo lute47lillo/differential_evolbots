764daaa02c7a67d58ff53b77fe8699cc71b29346
lute47lillo <lutelillocs47@gmail.com>
2024-04-10 13:57:49 -0400

    Simulation of multiple robots of a population

--- robot.py
+++ robot.py
@@ -4,92 +4,135 @@ class Robot:
     
     def __init__(self, springs, startingObjectPositions, max_steps):
         """
-        Initialize a new Robot instance.
+            Definition
+            -----------
+                Initialize a new Robot instance.
 
-        Parameters:
-        - name (str): The name of the robot.
-        - model (str): The model of the robot.
-        - color (str): The color of the robot.
+            Parameters
+            -----------
+                - springs (list): The springs and their information for the robot.
+                - startingObjectPositions (list): The starting positions of the objects of the robot.
+                - max_steps (int): Maximum number of steps for simulation of the robot.
+                
+            Returns
+            -----------
+                None
         """
-        self.loss = tai.field(dtype=tai.f32, shape=(), needs_grad=True) # 0-D tensor
         
+        # Create a field w/ max_steps X n_objects entries. It is stored in positions. Needs to be defined previously
+        real = tai.f32
+        tai.init(default_fp = real) # Init TAI
+        self.vec =  lambda: tai.Vector.field(2, dtype=real)  # Vector of length 2. Real Values
+        self.loss = tai.field(dtype=tai.f32, shape=()) # 0-D tensor
+        
+        # -----------------------------------------------------------------
+        
+        # Get common attributes -> Springs, objects and max_steps
         self.springs = springs
         self.startingObjectPositions = startingObjectPositions
         self.max_steps = max_steps
         
+        # Get number of .. for X robot
         self.n_objects = len(self.startingObjectPositions)
-        
-        # Create a field w/ max_steps X n_objects entries.
-        # It is stored in positions. Needs to be defined previously
-        #Vector of length 2. Real Values
-        real = tai.f32
-        tai.init(default_fp = real) # Init TAI
-        vec =  lambda: tai.Vector.field(2, dtype=real) 
-        
-        self.positions = vec()
-        self.velocities = vec()
-        self.spring_forces_on_objects = vec()
-    
-        # Store positions of every object at every time step.
-        # Where each position is a vector of length 2. x and y.
-        # positions = vec()
-        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.positions)
-
-        # Gradients of position. Changing as a function of the loss per time step.
-        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.positions.grad)
-
-        # velocities = vec()
-        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.velocities)
-        
-        # Forces acting on the objects
-        # spring_forces_on_objects = vec()
-        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.spring_forces_on_objects)
-
-        # You can add more attributes here as needed
         self.n_springs = len(self.springs)
+        
+        # -----------------------------------------------------------------
+        
+        # Create vectors for positions, velocity, and other physical attributes
+        self.positions = self.vec()
+        self.velocities = self.vec()
+        self.spring_forces_on_objects = self.vec()
     
-        # Store as Taichi fields
+        # Initialize them
+        self.init_robot_objects_ds()
+
+        # -----------------------------------------------------------------
+        
+        # Store as Taichi fields Spring physical attributes
         self.spring_anchor_a = tai.field(tai.i32)
         self.spring_anchor_b = tai.field(tai.i32)
         self.spring_at_rest_length = tai.field(tai.f32)
         self.spring_actuation = tai.field(tai.i32)
+        self.spring_restoring_forces = self.vec()
+        self.actuation = tai.field(tai.f32)
+        
+        # Initialize them
+        self.init_robot_springs_ds()
+        
+        # -----------------------------------------------------------------
+        
+        # Create Neural Network attributes
+        self.n_sensors = 10 + 4 * self.n_objects + 2
+        self.n_hidden_neurons = 32
+        self.weightsSH = tai.field(tai.f32)
+        self.weightsHM = tai.field(tai.f32)
+        self.hidden = tai.field(tai.f32)
+        self.bias_hidden = tai.field(tai.f32)
+    
+        # Initialize them
+        self.init_robot_weights_ds()
+        
+        # -----------------------------------------------------------------
+        
+        # Design center of mass of robot and goal
+        self.center = self.vec()
+        self.goal = self.vec()
+        
+        # Initialize
+        self.init_robot_center()
+        self.init_robot_goal()
+        
+        # -----------------------------------------------------------------
+        
+        # Set fields to requiere Grads
+        tai.root.lazy_grad()
+    
+        
+    def init_robot_springs_ds(self):
         
         # Capture motor value to be sent to every spring at every time_step
-        self.actuation = tai.field(tai.f32)
         tai.root.dense(tai.ij, (self.max_steps, self.n_springs)).place(self.actuation)
         
         # Taichi Structure for springs. Turn Spring anchor A & B from integer into field
         tai.root.dense(tai.i, self.n_springs).place(self.spring_anchor_a, self.spring_anchor_b, self.spring_at_rest_length, self.spring_actuation) 
 
         # Forces of the springs
-        self.spring_restoring_forces = vec()
         tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_springs).place(self.spring_restoring_forces)
-        
-        self.spring_anchors = (self.spring_anchor_a, self.spring_anchor_b)
-        
-        self.n_sensors = 10 + 4 * self.n_objects + 2
-        self.n_hidden_neurons = 32
+
     
+    def init_robot_objects_ds(self):
+        
+        # Store positions of every object at every time step.
+        # Where each position is a vector of length 2. x and y.
+        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.positions)
+
+        # Gradients of position. Changing as a function of the loss per time step.
+        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.positions.grad)
+
+        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.velocities)
+        
+        # Forces acting on the objects
+        tai.root.dense(tai.i, self.max_steps).dense(tai.j, self.n_objects).place(self.spring_forces_on_objects)
+    
+    def init_robot_weights_ds(self):
+        
         # Sensor to Hidden neurons and weights - Put weights from Sensors to hidden neurons
-        self.weightsSH = tai.field(tai.f32)
         tai.root.dense(tai.ij, (self.n_hidden_neurons, self.n_sensors)).place(self.weightsSH)
 
         # Hidden to Motor neurons and weights
-        self.weightsHM = tai.field(tai.f32)
         tai.root.dense(tai.ij, (self.n_springs, self.n_hidden_neurons)).place(self.weightsHM)
         
         # Create field for N hidden neurons at each time_step
-        self.hidden = tai.field(tai.f32)
         tai.root.dense(tai.ij, [self.max_steps, self.n_hidden_neurons]).place(self.hidden)
 
         # Create bias. One per each hidden neuron. Total N bias
-        self.bias_hidden = tai.field(tai.f32)
         tai.root.dense(tai.i, self.n_hidden_neurons).place(self.bias_hidden)
-        
-        
-        self.center = vec()
-        tai.root.dense(tai.i, self.max_steps).place(self.center)
-        
-        self.goal = vec()
+    
+    def init_robot_goal(self):
+    
         tai.root.place(self.goal)
-        tai.root.lazy_grad()
+
+    # TODO: Adapt based on the robot task
+    def init_robot_center(self):
+        
+        tai.root.dense(tai.i, self.max_steps).place(self.center)
--- simulate.py
+++ simulate.py
@@ -31,18 +31,11 @@ n_sin_waves = 10
 """
 # -----------------------------------------------------------------
 
-# Create a field w/ max_steps X n_objects entries.
-# It is stored in positions. Needs to be defined previously
-#Vector of length 2. Real Values
-real = tai.f32
-tai.init(default_fp = real) # Init TAI
-vec =  lambda: tai.Vector.field(2, dtype=real) 
-
 def n_sensors(n_objects):
     return n_sin_waves + 4 * n_objects + 2
 
-
 # -----------------------------------------------------------------
+
 def create_spring(springs_robot, i, j, is_motor, startingObjectPositions):
     """
         Definition
@@ -166,120 +159,9 @@ def create_population(n_robots_population):
         springs_population.append(springs)   
         startingObjectPositions_population.append(startingObjectPositions) 
     
-    return springs_population, startingObjectPositions_population
-
-# TODO: Aa placeholder - Create a Population of 2 robots
-# Delete old experimental population
-os.system("rm population/*.txt")
-springs_population, startingObjectPositions_population = create_population(2)   
-
-
-# Placeholder, get data of first robots  
-springs = springs_population[0]
-startingObjectPositions = startingObjectPositions_population[0]
-
-n_objects = len(startingObjectPositions)
-n_springs = len(springs)
+    return springs_population, startingObjectPositions_population 
 
 # -----------------------------------------------------------------
-
-# def init_robot_objects_ds(startingObjectPositions, vec):
-#     n_objects = len(startingObjectPositions)
-    
-#     # Store positions of every object at every time step.
-#     # Where each position is a vector of length 2. x and y.
-#     # positions = vec()
-#     tai.root.dense(tai.i, max_steps).dense(tai.j, n_objects).place(positions)
-
-#     # Gradients of position. Changing as a function of the loss per time step.
-#     tai.root.dense(tai.i, max_steps).dense(tai.j, n_objects).place(positions.grad)
-
-#     # velocities = vec()
-#     tai.root.dense(tai.i, max_steps).dense(tai.j, n_objects).place(velocities)
-    
-#     # Forces acting on the objects
-#     # spring_forces_on_objects = vec()
-#     tai.root.dense(tai.i, max_steps).dense(tai.j, n_objects).place(spring_forces_on_objects)
-
-    # return positions, velocities, spring_forces_on_objects
-
-# positions, velocities, spring_forces_on_objects = init_robot_objects_ds(startingObjectPositions, vec)
-
-# -----------------------------------------------------------------
-
-def init_robot_springs_ds(springs, vec):
-    n_springs = len(springs)
-    
-    # Store as Taichi fields
-    spring_anchor_a = tai.field(tai.i32)
-    spring_anchor_b = tai.field(tai.i32)
-    spring_at_rest_length = tai.field(tai.f32)
-    spring_actuation = tai.field(tai.i32)
-    
-    # Capture motor value to be sent to every spring at every time_step
-    actuation = tai.field(tai.f32)
-    tai.root.dense(tai.ij, (max_steps, n_springs)).place(actuation)
-    
-    # Taichi Structure for springs. Turn Spring anchor A & B from integer into field
-    tai.root.dense(tai.i, n_springs).place(spring_anchor_a, spring_anchor_b, spring_at_rest_length, spring_actuation) 
-
-    # Forces of the springs
-    spring_restoring_forces = vec()
-    tai.root.dense(tai.i, max_steps).dense(tai.j, n_springs).place(spring_restoring_forces)
-    
-    spring_anchors = (spring_anchor_a, spring_anchor_b)
-
-    return spring_anchors, spring_at_rest_length, spring_actuation, spring_restoring_forces, actuation
-
-# spring_anchors, spring_at_rest_length, spring_actuation, spring_restoring_forces, actuation = init_robot_springs_ds(springs, vec)
-# spring_anchor_a, spring_anchor_b = spring_anchors
-
-# -------------------------------------------------------------
-
-# TODO: Make controller also modifiable in size
-def init_robot_weights_ds(startingObjectPositions, n_hidden_neurons):
-    
-    # Attributes
-    n_objects = len(startingObjectPositions)
-    
-    # Sensor to Hidden neurons and weights - Put weights from Sensors to hidden neurons
-    weightsSH = tai.field(tai.f32)
-    tai.root.dense(tai.ij, (n_hidden_neurons, n_sensors(n_objects))).place(weightsSH)
-
-    # Hidden to Motor neurons and weights
-    weightsHM = tai.field(tai.f32)
-    tai.root.dense(tai.ij, (n_springs, n_hidden_neurons)).place(weightsHM)
-    
-    # Create field for N hidden neurons at each time_step
-    hidden = tai.field(tai.f32)
-    tai.root.dense(tai.ij, [max_steps, n_hidden_neurons]).place(hidden)
-
-    # Create bias. One per each hidden neuron. Total N bias
-    bias_hidden = tai.field(tai.f32)
-    tai.root.dense(tai.i, n_hidden_neurons).place(bias_hidden)
-    
-    return weightsSH, weightsHM, hidden, bias_hidden
-
-# weightsSH, weightsHM, hidden, bias_hidden = init_robot_weights_ds(startingObjectPositions, n_hidden_neurons)
-
-# -------------------------------------------------------------
-def init_robot_goal(vec):
-    
-    goal = vec()
-    tai.root.place(goal)
-    
-    return goal
-
-# TODO: Adapt based on the robot task
-def init_robot_center(vec):
-    
-    center = vec()
-    tai.root.dense(tai.i, max_steps).place(center)
-    
-    return center
-
-# -------------------------------------------------------------
-# -------------------------------------------------------------
 
 # Execute by Taichi and not python by using decorater
 @tai.kernel
@@ -288,8 +170,6 @@ def Compute_loss():
     # Focus on position of the objects to determine loss fn. Arbitrary choice
     # Second component of zeroth object. Loss = Height of 0th objects at last time_step
     loss[None] -= r.positions[r.max_steps-1, 0][1]
-    # r.loss[None] = loss[None]
-    # calculate the loss based on the distance travelled to the right
     
 # -------------------------------------------------------------
 
@@ -347,8 +227,6 @@ def Draw(frame_offset):
 
 # -------------------------------------------------------------
 
-# def Initialize(positions: tai.f32, goal, velocities, spring_anchors, spring_at_rest_length, spring_actuation,
-#                spring_restoring_forces, spring_forces_on_objects, hidden, actuation: tai.f32):
 def Initialize():
     """
         Definition
@@ -366,7 +244,7 @@ def Initialize():
         
     # spring_anchor_a, spring_anchor_b = spring_anchors
     for spring_idx in range(r.n_springs):
-        s = springs[spring_idx] # Get spring
+        s = r.springs[spring_idx] # Get spring
         r.spring_anchor_a[spring_idx]         = s[0] # the a object of that spring
         r.spring_anchor_b[spring_idx]         = s[1]
         r.spring_at_rest_length[spring_idx]   = s[2]
@@ -411,7 +289,7 @@ def Initialize_Neural_Network():
     """
     # Initialize sensor to hidden neurons
     for i in range(r.n_hidden_neurons):
-        for j in range(n_sensors(n_objects)):
+        for j in range(n_sensors(r.n_objects)):
             r.weightsSH[i,j] = np.random.randn() * 0.2 - 0.1
     
     # Init bias for hidden neurons
@@ -424,17 +302,12 @@ def Initialize_Neural_Network():
             r.weightsHM[i,j] = np.random.randn() * 0.2 - 0.1
             
 # -------------------------------------------------------------
-# def Simulate(positions: tai.f32, center: tai.f32, goal: tai.f32, velocities, weightsSH, weightsHM, bias_hidden, hidden, actuation: tai.f32, spring_pack):
+
 def Simulate():
 
     for time_step in range(1, r.max_steps):
         
-        # Update position of object.
-        # step_one(positions, center, goal, velocities, weightsSH, weightsHM, bias_hidden, hidden, actuation, spring_pack,time_step)
         step_one(time_step)
-        
-    print("Simulated")
-
 
 # -------------------------------------------------------------
 
@@ -466,8 +339,8 @@ def simulate_neural_network_SH(time_step: tai.i32):
             activation += 0.25 * r.weightsSH[i, j* 4 + 3 + n_sin_waves] * r.positions[time_step, j][1]
         
         # goal sensors -> how far the bot got?
-        activation += 0.25 * r.weightsSH[i, n_objects * 4 + n_sin_waves] * (r.goal[None][0] - r.center[time_step][0])
-        activation += 0.25 * r.weightsSH[i, n_objects * 4 + n_sin_waves + 1] * (r.goal[None][1] - r.center[time_step][1])
+        activation += 0.25 * r.weightsSH[i, r.n_objects * 4 + n_sin_waves] * (r.goal[None][0] - r.center[time_step][0])
+        activation += 0.25 * r.weightsSH[i, r.n_objects * 4 + n_sin_waves + 1] * (r.goal[None][1] - r.center[time_step][1])
             
         # Apply non-linearity
         activation += r.bias_hidden[i]
@@ -561,14 +434,8 @@ def simulate_objects(time_step: tai.i32):
         
 # -------------------------------------------------------------
 
-# def step_one(positions: tai.f32, center: tai.f32, goal: tai.f32, velocities, weightsSH, weightsHM, bias_hidden, hidden, actuation, spring_pack, time_step: tai.i32):
 def step_one(time_step: tai.i32):
-
-    # calculate_center_robot(positions, center, time_step)
-    # simulate_neural_network_SH(positions, center, goal, weightsSH, bias_hidden, hidden, time_step)
-    # simulate_neural_network_HM(weightsHM, hidden, actuation, time_step)
-    # simulate_springs(positions, spring_pack, actuation, time_step)
-    # simulate_objects(positions, velocities, spring_pack, time_step)
+    
     calculate_center_robot(time_step)
     simulate_neural_network_SH(time_step)
     simulate_neural_network_HM(time_step)
@@ -608,8 +475,8 @@ def tune_sh_weights():
             r.weightsSH[i, j* 4 + 3 + n_sin_waves]    -= learning_rate * r.weightsSH.grad[i, j* 4 + 3 + n_sin_waves]
         
         # goal sensors -> how far the bot got?
-        r.weightsSH[i, n_objects * 4 + n_sin_waves]       -= learning_rate * r.weightsSH.grad[i, r.n_objects * 4 + n_sin_waves]
-        r.weightsSH[i, n_objects * 4 + n_sin_waves + 1]   -= learning_rate * r.weightsSH.grad[i, r.n_objects * 4 + n_sin_waves + 1] 
+        r.weightsSH[i, r.n_objects * 4 + n_sin_waves]       -= learning_rate * r.weightsSH.grad[i, r.n_objects * 4 + n_sin_waves]
+        r.weightsSH[i, r.n_objects * 4 + n_sin_waves + 1]   -= learning_rate * r.weightsSH.grad[i, r.n_objects * 4 + n_sin_waves + 1] 
         
 def tune_robots_brain():
     
@@ -631,55 +498,29 @@ def Create_video():
 
 # -------------------------------------------------------------
 
-#TODO: Figure out a way of calculating for each robot.
-# Gradients - Loss
-
-
-r = Robot(springs, startingObjectPositions, max_steps)
-loss = tai.field(dtype=tai.f32, shape=(), needs_grad=True) # 0-D tensor
-tai.root.lazy_grad()
-
-def run_simulation():
+# Create population of robots
+n_robot_population = 2
+os.system("rm population/*.txt")
+springs_population, startingObjectPositions_population = create_population(n_robot_population)  
     
-    # Create population of robots
-    n_robot_population = 1
-    os.system("rm population/*.txt")
-    springs_population, startingObjectPositions_population = create_population(n_robot_population)  
+robot_drawing = []
+for robot_idx in range(n_robot_population):
+    print(f"Working on robot {robot_idx+1}")
     
-    robot_drawing = []
-    for robot_idx in range(n_robot_population):
+    # Get objects and springs individual robots
+    springs = springs_population[robot_idx]
+    startingObjectPositions = startingObjectPositions_population[robot_idx]
         
+    r = Robot(springs, startingObjectPositions, max_steps)
+    
+    # Create loss of that robot
+    loss = tai.field(dtype=tai.f32, shape=(), needs_grad=True) # 0-D tensor
+    tai.root.lazy_grad()
+    
+    for opt_step in range(2):        
         
-        # Get objects and springs individual robots
-        springs = springs_population[robot_idx]
-        startingObjectPositions = startingObjectPositions_population[robot_idx]
-        
-        # Init Objects
-        # positions, velocities, spring_forces_on_objects = init_robot_objects_ds(startingObjectPositions, vec)
-        
-        # Init Springs
-        # spring_anchors, spring_at_rest_length, spring_actuation, spring_restoring_forces, actuation = init_robot_springs_ds(springs, vec)
-        
-        # Init NNs parameters
-        # weightsSH, weightsHM, hidden, bias_hidden = init_robot_weights_ds(startingObjectPositions, n_hidden_neurons)
-        
-        # Determines center of the bot by the time_step
-        # center = init_robot_center(vec)
-
-        # Determine the goal of the robot.
-        # goal = init_robot_goal(vec)
-        
-        # Init NNs architecture
-        # Initialize_Neural_Network(n_objects, weightsSH, bias_hidden, weightsHM)
         Initialize_Neural_Network()
         Initialize()
-        
-        # Initialize body of the individual robot
-        # Initialize(positions, goal, velocities, spring_anchors, spring_at_rest_length, spring_actuation,
-            #    spring_restoring_forces, spring_forces_on_objects, hidden, actuation)
-        
-        # Pack it up for parameter readability 
-        # spring_pack = (spring_anchors, spring_at_rest_length, spring_actuation, spring_restoring_forces, spring_forces_on_objects)
         
         with tai.Tape(loss):
         
@@ -689,47 +530,20 @@ def run_simulation():
             
             Compute_loss()
             
-        print(loss[None])
+        r.loss = float(loss[None])
+        print(f"Robot {robot_idx} - Opt Step {opt_step+1}. Loss: {r.loss}")
         
         # Fine-tune the brain of the robot
         # tune_robots_brain(weightsHM, weightsSH, bias_hidden)
         tune_robots_brain()
         
-        # TODO: Eventually we might want to add the option to draw for every 'survival of the fittest' run.
-        
-        # robot_drawing.append([positions, spring_anchors])
-
-    # # TODO: OLD
-    # # Initialize the robot.
-    # Initialize()
-
-    # # Automated Differentiation.
-    # with tai.Tape(loss):
-        
-    #     Simulate(positions, velocities, weightsSH, weightsHM, bias_hidden, hidden, actuation, spring_pack,)
-        
-    #     loss[None] = 0.0
-    #     Compute_loss()
-
-    # print(loss[None])
-
-# Run simulation
-for opt_step in range(2):
+        if opt_step == 0:
+            os.system("rm images/*.png")
+            Draw(0)
+            
+    loss[None] = 0.0
     
-    run_simulation()
-    # robot_drawings = run_simulation()
-    # positions = robot_drawings[0][0]
-    # spring_anchors = robot_drawings[0][1]
-    
-    if opt_step == 0:
-        os.system("rm images/*.png")
-        Draw(0)
-
 Draw(max_steps)
-
     
-# os.system("rm images/*.png")
-# Draw(0)
-
 # Create the video
 Create_video()
