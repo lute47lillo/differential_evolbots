26886583522670a392bd645b90518e40e1442e2d
lute47lillo <lutelillocs47@gmail.com>
2024-05-03 21:28:39 -0400

    Final presentation ready v.1

--- baseline_simulate.py
+++ baseline_simulate.py
@@ -55,10 +55,13 @@ def draw_fittest_robot(r, img_dir, frame_offset, robot_index):
 @tai.kernel
 def compute_loss_baseline():
     
-    loss[None] -= (1.2 * (r.positions[r.max_steps-1, 0][0]) - 0.1) \
-                + (1.2 * (r.positions[r.max_steps-1, 1][0]) - 0.3) \
-                + (0.7 * (r.goal[None][0] - r.positions[r.max_steps-1, 0][0])) \
-                + (0.7 * (r.goal[None][0] - r.positions[r.max_steps-1, 1][0]))
+    # loss[None] -= (1.2 * (r.positions[r.max_steps-1, 0][0]) - 0.1) \
+    #             + (1.2 * (r.positions[r.max_steps-1, 1][0]) - 0.3) \
+    #             + (0.7 * (r.goal[None][0] - r.positions[r.max_steps-1, 0][0])) \
+    #             + (0.7 * (r.goal[None][0] - r.positions[r.max_steps-1, 1][0])) 
+    
+    loss[None] -= (1.2 * (r.positions[r.max_steps-1, 0][0] - 0.1)             + (r.positions[r.max_steps-1, 1][0] - 0.3)) \
+                - (0.7 * (r.goal[None][0] - r.positions[r.max_steps-1, 0][0]) + (r.goal[None][0] - r.positions[r.max_steps-1, 1][0]))
 
 
 """
@@ -75,6 +78,7 @@ if __name__ == "__main__":
     os.system(f"rm stats/baseline_loss_{variant_type}.txt")
     
     # Baseline simulation for test co-evolution or random Variant B for A/B testing.
+    # random should'nt draw initial, just after evolving to alleviate time.
     if variant_type == "random":
         # A/B Testing
         springs, startingObjectPositions = sim.simulate_robot(0, variant_type)
@@ -99,7 +103,7 @@ if __name__ == "__main__":
     # Define optimization steps based on original simulation
     baseline_opt_steps = (n_pop - 1) * n_opt // 2
     
-    for opt_step in range(n_pop):        
+    for opt_step in range(n_pop-1):        
             
         if opt_step == 0:
             sim.Initialize_Neural_Network(r)
